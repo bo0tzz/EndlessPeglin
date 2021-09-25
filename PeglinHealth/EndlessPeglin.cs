c2ea1211d0365b164e2d4da14c9a93cbb223874d
bo0tzz <btdewilligen@gmail.com>
2021-09-25 21:36:56 +0200

    Rename to EndlessPeglin

---
+++ PeglinHealth/EndlessPeglin.cs
@@ -0,0 +1,94 @@
+﻿using BepInEx;
+using HarmonyLib;
+using Relics;
+using System.Linq;
+
+namespace EndlessPeglin
+{
+    [BepInPlugin(modGuid, modName, modVersion)]
+    [BepInProcess("Peglin.exe")]
+    public class EndlessPeglin : BaseUnityPlugin
+    {
+        private const string modGuid = "me.bo0tzz.peglinmods.endless";
+        private const string modName = "Endless Peglin";
+        private const string modVersion = "1.0.0.0";
+        private readonly Harmony harmony = new Harmony(modGuid);
+
+        void Awake()
+        {
+            harmony.PatchAll();
+            Logger.LogInfo("Endless peglin mod loaded");
+        }
+    }
+    
+    [HarmonyPatch(typeof(GameInit), "Start")]
+    public class GameInitPatch
+    {
+        public static LoadMapData stolenLoadMapData;
+
+        public static void Prefix(LoadMapData ___LoadData)
+        {
+            if (___LoadData != null)
+            {
+                GameInitPatch.stolenLoadMapData = ___LoadData;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(DeckManager), "InstantiateDeck")]
+    public class DeckManagerInstantiatePatch
+    {
+        public static bool Prefix()
+        {
+            if (GameInitPatch.stolenLoadMapData != null && 
+                GameInitPatch.stolenLoadMapData.NewGame && 
+                UnityEngine.PlayerPrefs.GetInt(GameInit.BOSS_STREAK_PREF_STRING, 0) > 0)
+            {
+                UnityEngine.Debug.Log("Skipping deck instantiate");
+                return false;
+                
+            }
+            return true;
+        }
+    }
+    
+   
+    [HarmonyPatch(typeof(Relics.RelicManager), "Reset")]
+    public class RelicResetPatch
+    {
+        public static bool Prefix()
+        {
+            if (GameInitPatch.stolenLoadMapData != null && 
+                GameInitPatch.stolenLoadMapData.NewGame && 
+                UnityEngine.PlayerPrefs.GetInt(GameInit.BOSS_STREAK_PREF_STRING, 0) > 0)
+            {
+                UnityEngine.Debug.Log("Skipping relic reset");
+                return false;
+
+            }
+            return true;
+        }
+    }
+    /*
+     * 
+     [HarmonyPatch(typeof(PauseMenu), "Update")]
+     public class HealOnPausePatch
+     {
+         public static void Prefix()
+         {
+             UnityEngine.GameObject player = UnityEngine.GameObject.FindWithTag("Player");
+             player?.GetComponent<PlayerHealthController>()?.HealToFull();
+         }
+     }
+
+     [HarmonyPatch(typeof(BattleController), "DoAttack")]
+     public class StupidDamage
+     {
+         public static void Prefix(ref int ____damageBonus)
+         {
+             ____damageBonus = 650;
+         }
+     }
+    */
+
+}
