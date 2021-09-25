4294402771b8ddbb3a7450c5ee1ec6cd496a6d00
bo0tzz <btdewilligen@gmail.com>
2021-09-25 21:43:13 +0200

    Renaming fixes

--- PeglinHealth/Properties/AssemblyInfo.cs
+++ PeglinHealth/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PeglinHealth")]
+[assembly: AssemblyTitle("EndlessPeglin")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PeglinHealth")]
+[assembly: AssemblyProduct("EndlessPeglin")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
